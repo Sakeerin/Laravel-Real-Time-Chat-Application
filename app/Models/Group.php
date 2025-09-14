f77af1ef469aa98cd2e51897106212764572cabd
Sakeerin <theblue0800@gmail.com>
2025-09-14 10:29:43 +0700

    Implement Generate Seed Data

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -18,7 +18,7 @@ class Group extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class, 'group_users');
+        return $this->belongsToMany(User::class, 'group_user');
     }
 
     public function messages()
